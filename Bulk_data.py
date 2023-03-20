cfc15d1745c7ebd155234a0058a98efcb52da108
voonnaaravind <126200303+voonnaaravind@users.noreply.github.com>
2023-03-20 14:15:13 -0700

    Update Bulk_data.py
    Tested and Working Script

--- Bulk_data.py
+++ Bulk_data.py
@@ -1,17 +1,36 @@
-import re
+import logging
 from getpass import getpass
 from netmiko import ConnectHandler
 from netmiko.exceptions import NetMikoTimeoutException
 from netmiko.exceptions import AuthenticationException
 from netmiko.exceptions import SSHException
 
+import re
+
+
 
 username = input('Enter your username: ')
 password = getpass()
 
+logging.basicConfig(filename='netmiko_global.log', level=logging.DEBUG)
+logger = logging.getLogger("netmiko")
+
+#validation = '/home/a_avoonna/validation/'
+
+#with open('chg155077_config_file') as f:
+#       config_list = f.read().splitlines()
 
 with open('device_file') as f:
         cisco_ios_switch_list = f.read().splitlines()
+
+devices = []
+
+#clearing the old data from the CSV file and writing the headers
+f = open("ios.csv", "w+")
+f.write("IP Address, Hostname, Current_Version, Current_Image, Serial_Number, Device_Model")
+f.write("\n")
+f.close()
+
 
 for switch in cisco_ios_switch_list:
         print ('Connecting to device" ' + switch)
@@ -40,64 +59,32 @@ for switch in cisco_ios_switch_list:
         except Exception as unknow_error:
                 print('Some other error: ' + str(unknow_error))
                 continue
-        #list where informations will be stored
-        devices = []
 
+        #net_connect.save_config()
+        #show = net_connect.send_command('show run | i logging', read_timeout=200)
+        #print (show)
+        sh_ver_output = net_connect.send_command('show version')
+        #print(sh_ver_output)
+        regex_version = re.compile(r'Cisco\sIOS\sSoftware.+Version\s([^,]+)')
+        version = regex_version.findall(sh_ver_output)
+        regex_hostname = re.compile(r'(\S+)\suptime')
+        hostname = regex_hostname.findall(sh_ver_output)
+        regex_serial = re.compile(r'Processor\sboard\sID\s(\S+)')
+        serial = regex_serial.findall(sh_ver_output)
+        regex_ios = re.compile(r'System\simage\sfile\sis\s"([^ "]+)')
+        ios = regex_ios.findall(sh_ver_output)
+        regex_model = re.compile(r'[Cc]isco\s(\S+).*memory.')
+        model = regex_model.findall(sh_ver_output)
+        devices.append([ip_address_of_devices,hostname[0],version[0],ios[0],serial[0],model[0]])
+        #print(devices)
 
-        #clearing the old data from the CSV file and writing the headers
-        f = open("IOS.csv", "w+")
-        f.write("IP Address, Hostname, Uptime, Current_Version, Current_Image, Serial_Number, Device_Model, Device_Memory")
-        f.write("\n")
-        f.close()
-
-  # execute show version on router and save output to output object
-
-sh_ver_output = net_connect.send_command('show version')
-
-  #finding hostname in output using regular expressions
-
-regex_hostname = re.compile(r'(\S+)\suptime')
-hostname = regex_hostname.findall(sh_ver_output)
-
-  #finding uptime in output using regular expressions
-
-regex_uptime = re.compile(r'\S+\suptime\sis\s(.+)')
-uptime = regex_uptime.findall(sh_ver_output)
-uptime = str(uptime).replace(',' ,'').replace("'" ,"")
-uptime = str(uptime)[1:-1]
-
-
-  #finding version in output using regular expressions
-
-regex_version = re.compile(r'Cisco\sIOS\sSoftware.+Version\s([^,]+)')
-version = regex_version.findall(sh_ver_output)
-  #finding serial in output using regular expressions
-
-regex_serial = re.compile(r'Processor\sboard\sID\s(\S+)')
-serial = regex_serial.findall(sh_ver_output)
-  #finding ios image in output using regular expressions
-
-regex_ios = re.compile(r'System\simage\sfile\sis\s"([^ "]+)')
-ios = regex_ios.findall(sh_ver_output)
-  #finding model in output using regular expressions
-
-regex_model = re.compile(r'[Cc]isco\s(\S+).*memory.')
-model = regex_model.findall(sh_ver_output)
-
-  #finding the router's memory using regular expressions
-
-regex_memory = re.search(r'with (.*?) bytes of memory', sh_ver_output).group(1)
-memory = regex_memory
-
-  #append results to table [hostname,uptime,version,serial,ios,model]
-
-devices.append([ip_address_of_devices, hostname[0],uptime,version[0],ios[0], serial[0],model[0], memory])
-
-  #print all results (for all routers) on screen
+net_connect.disconnect()
 
 for i in devices:
     i = ", ".join(i)
-    f = open("IOS.csv", "a")
+    f = open("ios.csv", "a+", newline="")
     f.write(i)
     f.write("\n")
     f.close()
+
+
